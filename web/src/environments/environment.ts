ead369692dd2fc1f0418e3140ad4df6b7ed14a95
Leandro Luna <leandro.j.luna@gmail.com>
2024-08-24 16:29:05 -0300

    update prod api

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://fracture-vision-api-homo.us-east-1.elasticbeanstalk.com'
+    baseUrl: 'http://homo.us-east-1.elasticbeanstalk.com'
 };
